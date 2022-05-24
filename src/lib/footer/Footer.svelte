07f6daa2cef4cd37cbda36297c63a3e1be3cfe44
ajhrtmn <ajhrtmn.mail@gmail.com>
2022-05-24 15:15:01 +0000

    Clean up and create boilerplate start.

---
+++ src/lib/footer/Footer.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<footer><nav><ul><li /></ul></nav></footer>
+
+<style>
+	footer {
+		display: flex;
+		justify-content: space-between;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,14 +9,14 @@
 	<title>Home</title>
 </svelte:head>
 
-<section />
+<section
+	class="w-full h-full flex flex-col items-center justify-center bg-gray-400"
+>
+	<div class="p-8 bg-white rounded-xl shadow-lg">The Card</div>
+</section>
 
 <style>
 	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
+		min-height: 100vh;
 	}
 </style>
